94dd2b0a121489dc47b812143b880bac5661721c
keiaz <Kei@Sugyeongui-MacBook-Air.local>
2016-08-25 13:31:50 +0900

    Initial commit

---
+++ app/src/main/java/com/hanyangraon/kei/app_hnro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hanyangraon.kei.app_hnro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
